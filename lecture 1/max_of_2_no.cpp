3538016e38f002375f5ba5358c849400f7bb92a9
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-16 22:36:53 +0530

    notes

---
+++ lecture 1/max_of_2_no.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the value of a";
+    cin>>a;
+    cout<<"enter the value of b";
+    cin>>b;
+    if(a>b){
+        cout<<"a is maximum"<<a;
+    }
+    else{
+        cout<<"b is max"<<b;
+    }
+    return 0;
+}
