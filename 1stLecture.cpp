011f1c1bfeb61c47ba124d06b1ab0af21f00ff8c
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-16 10:35:31 +0530

    Pattern

--- 1stLecture.cpp
+++ 1stLecture.cpp
@@ -8,5 +8,20 @@ int main() {
 	c=a+b;
 	cout<<"The sum of a and b is: "<<c<<endl;
 
+	
+
+// #include <iostream>
+// using namespace std;
+// int main()
+
+	int si,p,t,r;
+	cout<<"Enter the principal amount: ";
+	cin>>p;
+	cout<<"Enter the time in years: ";
+	cin>>t;
+	cout<<"Enter the rate of interest: ";
+	cin>>r;
+	si=(p*t*r)/100;
+	cout<<"The simple interest is: "<<si<<endl;
 	return 0;
 }
