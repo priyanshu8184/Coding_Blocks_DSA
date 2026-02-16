2f66be2b353cd56f0b68091fa33b19e3324f44fa
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-16 11:44:28 +0530

    Notes

--- lecture2_pattern.cpp
+++ lecture2_pattern.cpp
@@ -12,12 +12,42 @@ int main(){
     cout<<"Enter the number of rows: ";
     cin>>n;
     for ( int i = 0; i < n; i++){
-        for ( int j = 0; j < n; j++)
+        for ( int j = 0; j < i; j++)
         {
             cout<<" * ";
         }
         cout<<endl;
     }
+    cout<<"enter the number of rows";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            // space
+            cout <<"  ";
+        }
+        for(int k=1;k<=i;k++){
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=i-1;j++){
+            // space
+            cout <<"  ";
+        }
+        for(int k=1;k<=n-i+1;k++){
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+    
+
     return 0;
     
 }
