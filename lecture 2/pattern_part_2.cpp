69000afab8104f67fe01c83e28350f57c21a314f
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-17 10:39:07 +0530

    Notes

--- lecture 2/pattern_part_2.cpp
+++ lecture 2/pattern_part_2.cpp
@@ -5,13 +5,13 @@ int main(){
     int n;
     cout<<"enter the value of n";
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*i-2;j++){
             // space
-            cout<<" ";
+            cout<<"   ";
         }
         // star printing
-        for(int j=0;j<n-i;j++){
+        for(int j=1;j<=n-i+1;j++){
             cout<<"* ";
         }
         cout<<endl;
