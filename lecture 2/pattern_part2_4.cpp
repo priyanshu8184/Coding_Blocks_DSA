73ec79676306bbc3c1a39facc53db15e031e65d7
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-17 20:23:29 +0530

    Notes

--- lecture 2/pattern_part2_4.cpp
+++ lecture 2/pattern_part2_4.cpp
@@ -6,6 +6,18 @@ int main(){
     cout<<"Enter the value of n";
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i)
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            if(j%2==0){
+                cout<<"* ";
+            }
+            else{
+                cout<<"! ";
+            }
+        }
+        cout<<endl;
     }
+    return 0;
 }
