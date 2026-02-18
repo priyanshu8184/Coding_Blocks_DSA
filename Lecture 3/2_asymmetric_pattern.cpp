00d5b684f7a3be269b5cb6636a5b47aef5341186
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-18 10:49:11 +0530

    Pattern

--- Lecture 3/2_asymmetric_pattern.cpp
+++ Lecture 3/2_asymmetric_pattern.cpp
@@ -1,11 +1,10 @@
-//  row symmetric patterns
+//  row asymmetric patterns
 // * 
 // * *
 // * * *
 // * * * *
 // * * * * *
-// * * * * *
-// * * * *
+// * * * * 
 // * * *
 // * *
 // *
@@ -23,7 +22,7 @@ using namespace std;
         cout<<endl;
     }
     // lower row
-    for(int i=n;i>=1;i--){
+    for(int i=n-1;i>=1;i--){
         for(int j=1;j<=i;j++){
             cout<<"* ";
         }
