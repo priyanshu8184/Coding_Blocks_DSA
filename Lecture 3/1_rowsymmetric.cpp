a694002ad7790329ed229353148baeb0b87dd6ab
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-18 10:42:52 +0530

    notes

---
+++ Lecture 3/1_rowsymmetric.cpp
@@ -0,0 +1,21 @@
+//  row symmetric patterns
+#include<iostream>
+using namespace std;
+ int main(){
+    int n;
+    cout<<"enter the value of n";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
--- lecture 2/pattern_part2_4.cpp
+++ lecture 2/pattern_part2_4.cpp
@@ -10,7 +10,7 @@ int main(){
             cout<<"  ";
         }
         for(int j=1;j<=2*i-1;j++){
-            if(j%2==0){
+            if(j%2!=0){
                 cout<<"* ";
             }
             else{
