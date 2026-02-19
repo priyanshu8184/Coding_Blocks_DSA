c0fd5ce6622594517e221a383c9b733268dd4d0e
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-19 11:39:00 +0530

    Patterns

--- Lecture 3/Pattern_5.cpp
+++ Lecture 3/Pattern_5.cpp
@@ -5,5 +5,29 @@ int main(){
     int n;
     cout<<"Enter the number of rows:";
     cin>>n;
-    
+    for(int i=1;i<=n;i++){
+        // space
+        // a loop
+
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        //star
+        // b loop
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        } 
+        //b loop 
+        for(int j=2;j<=i;j++) 
+        // here you can write 1 also on the place of 2{
+            cout<<"* ";
+        } 
+        // a loop
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
 }
