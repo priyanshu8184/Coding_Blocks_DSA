a3822d289dac890c9f0733b9247d02fdbf51367d
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-24 11:31:56 +0530

    pattern

--- Lecture 3/pattern_7.cpp
+++ Lecture 3/pattern_7.cpp
@@ -1,9 +1,47 @@
 // pattern number 7
+// * * *     * * * 
+// * *         * * 
+// *             * 
+
+// *             * 
+// * *         * * 
+// * * *     * * * 
 #include<iostream> 
 using namespace std;
 int main(){
     int n;
     cout<<"Enter the number of rows:";
     cin>>n;
-    for(int i=)
-},.
+    int m=(n+1)/2;
+    for(int i=1;i<=m;i++){
+        // left star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        // spaces
+        for(int j=1;j<=2*i;j++){
+            cout<<"  ";
+        }
+        // right star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=m-1;i>=1;i--){
+        // left star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        // spaces
+        for(int j=1;j<=2*i;j++){
+            cout<<"  ";
+        }
+        // right star
+        for(int j=1;j<=m-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
